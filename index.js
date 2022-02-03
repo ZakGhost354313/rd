f918f6ab557359545873320ef3bd903b2b24e391
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2022-02-03 10:12:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const doc = document.getElementById('redirect');
 
 const href;
 
+var _html = href + ' , if you are not redirected by then <a href=' + href + '>click me</a>'
+
+doc.innerHTML += _html
+
+
 window.location = href;
